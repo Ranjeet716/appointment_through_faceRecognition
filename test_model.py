c4236443ae9982d6ed3a91b1ca1c3c496adbccae
Ranjeet Kumar Yadav <51926901+Ranjeet716@users.noreply.github.com>
2020-05-17 21:41:47 +0530

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -3,36 +3,36 @@ import os
 import numpy as np
 import recognizer as r
 
+#just to check Whether our recognizer work or not
 r.check()
 
 test_img=cv2.imread('TestImages/frame0.jpg')
 faces_detected,gray_img=r.faceDetection(test_img)
 print("faces_detected is:",faces_detected)
-print("And its Gray form is ",gray_img)
+#print("And its Gray form is ",gray_img)
 
-
-#Comment belows lines when running this program second time.Since it saves training.yml file in directory
+#we can comment the following code,if we want to use the whole code futher it further...as it is saved as training.yml
 faces,sub_dir=r.image_level('trainingImages')
 face_recognizer=r.train_classifier(faces,sub_dir)
 face_recognizer.write('trainingData.yml')
 
 
 #Uncomment below line for subsequent runs
-#face_recognizer=cv2.face.LBPHFaceRecognizer_create()
-#face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
 
-name={0:"Priyanka",1:"Kangana",2:"ranjeet"}#creating dictionary containing names for each label
-#x and y are starting cordinate and L=x+w is how long it will be and H= y+h is how high the image will be
+name={1:"ranjeet"}
+#x and y are starting coordinate and L=x+w is how long it will be(Lenght alog x-axis) and H= y+h is how high the image will be
 
 for face in faces_detected:
     (x,y,w,h)=face
     roi_gray=gray_img[y:y+h,x:x+h]
-    label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+    label,confidence=face_recognizer.predict(roi_gray)
     print("confidence:",confidence)
     print("label:",label)
     r.draw_rect(test_img,face)
     predicted_name=name[label]
-    if(confidence>60):#If confidence more than 37 then don't print predicted face text on screen
+    if(confidence>60):
         continue
     r.put_text(test_img,predicted_name,x,y)
 
